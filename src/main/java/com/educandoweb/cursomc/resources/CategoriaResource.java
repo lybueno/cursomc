92720c2489eb1dea242e6b08aba8d1983f69c8f8
Lygia Bueno <lybueno@gmail.com>
2022-08-12 15:12:32 -0300

    Testando classe de domínio - Categoria

--- src/main/java/com/educandoweb/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/educandoweb/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,24 @@
 package com.educandoweb.cursomc.resources;
 
+import com.educandoweb.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando!";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+        List<Categoria> lista = new ArrayList<>();
+        lista.addAll(Arrays.asList(cat1, cat2));
+        return lista;
     }
 }
