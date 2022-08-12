0ba0daf966538f9afa1156a5473c50d0e0c36381
Lygia Bueno <lybueno@gmail.com>
2022-08-12 15:24:18 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/educandoweb/cursomc/domain/Categoria.java
+++ src/main/java/com/educandoweb/cursomc/domain/Categoria.java
@@ -1,9 +1,18 @@
 package com.educandoweb.cursomc.domain;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Categoria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 
